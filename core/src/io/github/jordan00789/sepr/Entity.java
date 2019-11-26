287ebbf7f5c8b9e48024d3703061323f34031d32
glnkvn <56406782+glnkvn@users.noreply.github.com>
2019-11-26 13:30:42 +0000

    Update Entity.java

--- core/src/io/github/jordan00789/sepr/Entity.java
+++ core/src/io/github/jordan00789/sepr/Entity.java
@@ -1,5 +1,24 @@
 package io.github.jordan00789.sepr;
 
 public class Entity {
+	
+	private float health;
+
+	public float getHealth() {
+		return health;
+	}
+
+	public void setHealth(float health) {
+		this.health = health;
+	}
+	
+	public float takeDamage(int damage) {
+		return health-damage;
+	}
+	
+	public boolean isDestroyed() {
+		return (getHealth() <= 0.0);
+	}
+	
 
 }
