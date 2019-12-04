07ca63ea9a8cdba9af75d9a81e60a1a7c4f3da03
Jordan <jordan00789@gmail.com>
2019-12-04 16:41:43 +0000

    Kroy now inherits from game; removed old code.

--- core/src/io/github/jordan00789/sepr/Kroy.java
+++ core/src/io/github/jordan00789/sepr/Kroy.java
@@ -1,81 +1,34 @@
 package io.github.jordan00789.sepr;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import java.lang.Math;
 
-public class Kroy extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	Sprite truck;
-	float truckx = 0;
-	float trucky = 0;
-	float trucka = 0.1f;
-	float truckdecel = 2;
-	float truckxv, truckyv;
+public class Kroy extends Game {
 
-	@Override
+	public SpriteBatch batch;
+	public BitmapFont font;
+
 	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("firetruck.png");
-		truck = new Sprite(img, 512, 512);
-		truck.setScale(0.25f);
-		truck.setOrigin(256, 320);//Drift mode
+		font = new BitmapFont();
+		this.setScreen(new MainMenu(this));
 	}
 
-	@Override
 	public void render() {
-		if (Gdx.input.isKeyPressed(Keys.UP)) {
-			truckyv += Gdx.graphics.getDeltaTime() + trucka;
-		}
-		if (Gdx.input.isKeyPressed(Keys.DOWN)) {
-			truckyv -= Gdx.graphics.getDeltaTime() + trucka;
-		}
-		if (Gdx.input.isKeyPressed(Keys.LEFT)) {
-			truckxv -= Gdx.graphics.getDeltaTime() + trucka;
-		}
-		if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
-			truckxv += Gdx.graphics.getDeltaTime() + trucka;
-		}
-		trucky += truckyv;
-		truckx += truckxv;
-
-		if (truckyv>0) {
-			truckyv -= trucka/truckdecel;
-		}
-
-		if (truckyv<0) {
-			truckyv += trucka/truckdecel;
-		}
-
-		if (truckxv>0) {
-			truckxv -= trucka/truckdecel;
-		}
-		if (truckxv<0) {
-			truckxv += trucka/truckdecel;
-		}
-
-
-		Gdx.gl.glClearColor(0.8f, 1, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		truck.setPosition(truckx, trucky);
-		truck.setRotation((float) (Math.atan2(-truckxv, truckyv)*180/Math.PI));
-		truck.draw(batch);
-		batch.end();
+		super.render();
 	}
 
-
-
-	@Override
 	public void dispose() {
 		batch.dispose();
-		img.dispose();
+		font.dispose();
 	}
 }
