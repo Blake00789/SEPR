a38ce6894f0dc17d9f23af3c4a594876ef86fa23
Jordan <jordan00789@gmail.com>
2019-12-04 18:49:33 +0000

    basic box2d physics code

--- core/src/io/github/jordan00789/sepr/MainGame.java
+++ core/src/io/github/jordan00789/sepr/MainGame.java
@@ -4,16 +4,31 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
+
+import java.awt.*;
 
 public class MainGame implements Screen {
     final Kroy game;
     OrthographicCamera camera;
+    Firetruck truck;
+    World world;
+    Box2DDebugRenderer debugRenderer;
 
     public MainGame(final Kroy game) {
         this.game = game;
 
+
         camera = new OrthographicCamera();
-        camera.setToOrtho(false, 1920, 1080);
+
+        Box2D.init();
+        World world = new World(new Vector2(0, 0), true);
+        Box2DDebugRenderer debugRenderer = new Box2DDebugRenderer();
+
+        truck = new Firetruck(100, new Point(600,600), 0);
     }
 
     public void render(float delta) {
@@ -25,7 +40,11 @@ public class MainGame implements Screen {
 
         game.batch.begin();
         game.font.draw(game.batch, "My test text", 480, 480);
+        game.batch.draw(truck.getTexture(), truck.getPos().x, truck.getPos().y);
         game.batch.end();
+
+        debugRenderer.render(world, camera.combined);
+        world.step(1/60f, 6, 2);
     }
 
     @Override
