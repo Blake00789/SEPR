3182a6ff62180e1f11eb7d5ab41e82dade74031f
Jordan <jordan00789@gmail.com>
2019-12-04 16:36:17 +0000

    switched over from lwjgl to lwjgl3

--- desktop/src/io/github/jordan00789/sepr/desktop/DesktopLauncher.java
+++ desktop/src/io/github/jordan00789/sepr/desktop/DesktopLauncher.java
@@ -1,14 +1,17 @@
 package io.github.jordan00789.sepr.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import io.github.jordan00789.sepr.Kroy;
+
+import java.awt.*;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1920;
-		config.height = 1080;
-		new LwjglApplication(new Kroy(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Kroy");
+		config.setFullscreenMode(Gdx.graphics.getDisplayMode());
+		new Lwjgl3Application(new Kroy(), config);
 	}
 }
