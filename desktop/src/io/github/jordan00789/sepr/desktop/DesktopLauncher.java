6ea2f7bcaa2d0f36db77a42fd2d5d9db03cee7b1
sch556 <sch556@york.ac.uk>
2019-12-05 14:32:49 +0000

    Updating to lwjgl3

--- desktop/src/io/github/jordan00789/sepr/desktop/DesktopLauncher.java
+++ desktop/src/io/github/jordan00789/sepr/desktop/DesktopLauncher.java
@@ -1,14 +1,14 @@
 package io.github.jordan00789.sepr.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import io.github.jordan00789.sepr.Kroy;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.width = 1920;
 		config.height = 1080;
-		new LwjglApplication(new Kroy(), config);
+		new Lwjgl3Application(new Kroy(), config);
 	}
 }
