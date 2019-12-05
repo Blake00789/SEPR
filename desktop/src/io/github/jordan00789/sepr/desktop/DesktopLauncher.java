f9b199fb259e4a48390d699cc14cc041aa79cfb2
sch556 <sch556@york.ac.uk>
2019-12-05 14:40:31 +0000

    small commit

--- desktop/src/io/github/jordan00789/sepr/desktop/DesktopLauncher.java
+++ desktop/src/io/github/jordan00789/sepr/desktop/DesktopLauncher.java
@@ -7,8 +7,8 @@ import io.github.jordan00789.sepr.Kroy;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.width = 1920;
-		config.height = 1080;
+		config.setTitle("Kroy");
+		config.setWindowedMode(1920, 1080);
 		new Lwjgl3Application(new Kroy(), config);
 	}
 }
