2a38bf5faf0cc9de528ba5afcc3b8debf0259f42
Tamour99 <56805857+Tamour99@users.noreply.github.com>
2019-12-06 17:06:57 +0000

    added menus

--- core/src/io/github/jordan00789/sepr/Kroy.java
+++ core/src/io/github/jordan00789/sepr/Kroy.java
@@ -4,22 +4,37 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 
 import java.lang.Math;
 
 public class Kroy extends Game {
 
+	public static final int V_WIDTH = 1920;
+	public static final int V_HEIGHT = 1080;
+
+
+	public OrthographicCamera camera;
 	public SpriteBatch batch;
 	public BitmapFont font;
+	public AssetManager assets;
 
 	public void create() {
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 1080,1920);
 		batch = new SpriteBatch();
 		font = new BitmapFont();
+
+		initFonts();
+
 		this.setScreen(new MainMenu(this));
 	}
 
@@ -30,5 +45,15 @@ public class Kroy extends Game {
 	public void dispose() {
 		batch.dispose();
 		font.dispose();
+		assets.dispose();
+	}
+
+	private void initFonts() {
+		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/Bubblegum.ttf"));
+		FreeTypeFontGenerator.FreeTypeFontParameter params = new FreeTypeFontGenerator.FreeTypeFontParameter();
+
+		params.size = 24;
+		params.color = Color.PINK;
+		font = generator.generateFont(params);
 	}
 }
--- core/src/io/github/jordan00789/sepr/MainMenu.java
+++ core/src/io/github/jordan00789/sepr/MainMenu.java
@@ -5,28 +5,58 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class MainMenu implements Screen {
 
-    final Kroy game;
+    private final Kroy game;
+    private Stage stage;
+    private Texture img;
+    private SpriteBatch batch;
 
-    OrthographicCamera camera;
+    private ShapeRenderer shapeRenderer; //
+
+    public OrthographicCamera camera;
 
     public MainMenu(final Kroy game) {
         this.game = game;
+        this.stage = new Stage(new FitViewport(Kroy.V_WIDTH, Kroy.V_HEIGHT, game.camera));
+        this.shapeRenderer = new ShapeRenderer();
+        Gdx.input.setInputProcessor(stage);
 
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, 1920, 1080);
+        img = new Texture("SpaceKey.jpg");
+        batch = new SpriteBatch();
+
         //Menu branch
+    }
+
+    @Override
+    public void show() {
+        System.out.println("Home Screen");
+        Gdx.input.setInputProcessor(stage);
+        stage.clear();
+    }
+
+
+    public void update(float delta){
+        stage.act(delta);
     }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0.2f, 1);
-        Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img,0,0);
+        batch.end();
 
-        camera.update();
-        game.batch.setProjectionMatrix(camera.combined);
+        update(delta);
+
+        stage.draw();
 
         game.batch.begin();
         game.font.draw(game.batch, "Welcome to Kroy!!! ", 100, 150);
@@ -41,15 +71,15 @@ public class MainMenu implements Screen {
 
     @Override
     public void resize(int width, int height) {
+        stage.getViewport().update(width, height, false);
     }
 
-    @Override
-    public void show() {
-    }
 
     @Override
     public void hide() {
     }
+
+
 
     @Override
     public void pause() {
@@ -61,5 +91,7 @@ public class MainMenu implements Screen {
 
     @Override
     public void dispose() {
+        batch.dispose();
+        img.dispose();
     }
 }
