b24c4c11797495692bbaf6bb7bcd0d0dbe6e21c5
sch556 <sch556@york.ac.uk>
2019-12-29 16:32:07 +0000

    Changes to Firetrucks, added test branch
    Firetrucks now have direction-based movement instead of vector-based.
    A new test folder has been created and populated with tests for
    firetrucks.

--- core/src/io/github/jordan00789/sepr/Firetruck.java
+++ core/src/io/github/jordan00789/sepr/Firetruck.java
@@ -1,95 +1,164 @@
 package io.github.jordan00789.sepr;
 
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 
-public class Firetruck extends Entity implements Moveable,Attack {
+public class Firetruck extends Entity implements Moveable, Attack {
 
 	private int water;
 	private int maxWater;
-	float acceleration = 0.1f;
-	float deceleration = 2;
-	private int direction = 0;
+	private float acceleration = 2;
+	private float deceleration = 0.2f;
+	private float direction = 0;
 	private float velocity = 0;
 
+	// These will be used in the attack method
 	private static int range = 10;
 	private static int flowRate = 5;
 
+	/**
+	 * Creates a Firetruck sprite using the texture provided, with the specified
+	 * amounts of health and water.
+	 * 
+	 * @param health   The amount of health the truck has.
+	 * @param maxWater The maximum amount of water in the truck.
+	 * @param texture  The texture given to the Firetruck sprite.
+	 */
 	public Firetruck(int health, int maxWater, Texture texture) {
 		super(health, texture);
 		this.maxWater = water = maxWater;
 	}
 
+	/** @return The truck's current amount of water. */
 	public int getWater() {
 		return water;
 	}
 
+	/**
+	 * Sets the amount of water in the truck if the amount is valid.
+	 * 
+	 * @param amount The amount that the water variable is set to.
+	 */
 	private void setWater(int amount) {
 		if (amount <= maxWater && amount > 0) {
 			water = amount;
 		}
 	}
 
-	public void refill() {
-		setWater(maxWater);
-	}
-
+	/**
+	 * Removes the specified amount of water from the truck.
+	 * 
+	 * @param amount The amount of water removed from the truck.
+	 */
 	public void takeWater(int amount) {
 		if (amount <= water && amount > 0) {
 			water -= amount;
 		}
 	}
-	
-	private void setDirection(int direction) {
-		this.direction = direction;
+
+	/**
+	 * Refills the truck to the maximum amount of water.
+	 */
+	public void refill() {
+		setWater(maxWater);
 	}
-	
-	public int getDirection() {
+
+	/**
+	 * Sets the trucks direction.
+	 * 
+	 * @param direction The value in degrees the truck's direction is set to.
+	 * @throws RuntimeException If the value is invalid, a runtime exception is
+	 *                          thrown.
+	 */
+	private void setDirection(float direction) {
+		if (direction >= 0 && direction <= 360) {
+			this.direction = direction;
+		} else {
+			throw new RuntimeException();
+		}
+	}
+
+	/** @return The truck's current direction. */
+	public float getDirection() {
 		return direction;
 	}
-	
+
+	/**
+	 * Sets the truck's velocity.
+	 * 
+	 * @param velocity The value the velocity is set to.
+	 */
 	private void setVelocity(float velocity) {
 		this.velocity = velocity;
 	}
-	
+
+	/** @return The truck's current velocity. */
 	public float getVelocity() {
 		return velocity;
 	}
-	
+
+	/** Turns the truck left. */
 	public void turnLeft() {
 		if (getDirection() == 0) {
 			setDirection(360);
 		} else {
-			setDirection(getDirection()-1);
+			setDirection(getDirection() - getTurnSpeed());
 		}
 	}
 
+	/** Turns the truck right. */
 	public void turnRight() {
 		if (getDirection() == 360) {
 			setDirection(0);
 		} else {
-			setDirection(getDirection()+1);
+			setDirection(getDirection() + getTurnSpeed());
 		}
 	}
-	
+
+	/**
+	 * Calculates the current turning radius of the truck.
+	 * 
+	 * @return Turning speed.
+	 */
+	private float getTurnSpeed() {
+		if (velocity > 50f) {
+			return (5 / velocity);
+		} else {
+			return 0.1f;
+		}
+	}
+
+	/** Moves the truck forward. */
 	public void goForward() {
-		setVelocity(getVelocity()+acceleration);
+		setVelocity(getVelocity() + acceleration);
 	}
-	
+
+	/** Moves the truck backward. */
 	public void goBackward() {
-		setVelocity(getVelocity()-acceleration);
+		setVelocity(getVelocity() - acceleration);
 	}
-	
+
+	/**
+	 * Updates the truck's position and rotation.
+	 * 
+	 * @param delta The current delta time.
+	 */
 	public void update(float delta) {
-		setX((float)((getX() + Math.sin(direction))*delta*velocity));
-		setY((float)((getY() + Math.cos(direction))*delta*velocity));
+		if (velocity > 0.01f) {
+			velocity -= deceleration;
+		} else if (velocity < 0.01f) {
+			velocity += deceleration;
+		} else {
+			velocity = 0;
+		}
+		setRotation(-direction * (float) (180 / Math.PI));
+		setX((float) (getX() + (Math.sin(direction) * delta * velocity)));
+		setY((float) (getY() + (Math.cos(direction) * delta * velocity)));
 	}
 
 	@Override
 	public void attack() {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 }
--- core/src/io/github/jordan00789/sepr/Kroy.java
+++ core/src/io/github/jordan00789/sepr/Kroy.java
@@ -1,24 +1,15 @@
 package io.github.jordan00789.sepr;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.Fixture;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
-import java.awt.Point;
-import java.lang.Math;
 
 public class Kroy extends Game {
 	SpriteBatch batch;
@@ -36,7 +27,7 @@ public class Kroy extends Game {
 		
 		truck1 = new Firetruck(200, 100, img);
 		truck1.setScale(0.25f);
-		truck1.setOrigin(256, 320);//Drift mode
+		truck1.setOrigin(256, 256);
 		
 		truck2 = new Firetruck(200, 100, img);
 		truck2.setScale(0.3f);
@@ -45,22 +36,24 @@ public class Kroy extends Game {
 		currentTruck = truck1;
 		currentTruck.setColor(Color.RED);
 		
-		world = new World(new Vector2(0, -98f), true);
-		BodyDef bodyDef = new BodyDef();
-	    bodyDef.type = BodyDef.BodyType.DynamicBody;
-	    bodyDef.position.set(truck1.getX(), truck1.getY());
-	    truck = world.createBody(bodyDef);
-	    PolygonShape shape = new PolygonShape();
-	    shape.setAsBox(truck1.getWidth()/2, truck1.getHeight()/2);
-	    FixtureDef fixtureDef = new FixtureDef();
-        fixtureDef.shape = shape;
-        fixtureDef.density = 1f;
-        Fixture fixture = truck.createFixture(fixtureDef);
-        shape.dispose();
 	}
 
 	@Override
 	public void render() {
+		takeInputs();
+
+		Gdx.gl.glClearColor(0.8f, 1, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		truck1.update(Gdx.graphics.getDeltaTime());
+		truck2.update(Gdx.graphics.getDeltaTime());
+		
+		truck1.draw(batch);
+		truck2.draw(batch);
+		batch.end();
+	}
+	
+	private void takeInputs() {
 		if (Gdx.input.isKeyPressed(Keys.UP)) {
 			currentTruck.goForward();
 		}
@@ -73,27 +66,24 @@ public class Kroy extends Game {
 		if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
 			currentTruck.turnRight();
 		}
-		
 		if (Gdx.input.isKeyPressed(Keys.SPACE)) {
 			currentTruck.takeWater(1);
 		}
-		
-		if (Gdx.input.isKeyPressed(Keys.NUM_1)) {currentTruck.setColor(Color.WHITE);currentTruck = truck1;currentTruck.setColor(Color.RED);}
-		if (Gdx.input.isKeyPressed(Keys.NUM_2)) {currentTruck.setColor(Color.WHITE);currentTruck = truck2;currentTruck.setColor(Color.RED);}
-		
-		
-		world.step(Gdx.graphics.getDeltaTime(), 6, 2);
-
-		Gdx.gl.glClearColor(0.8f, 1, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		truck1.update(Gdx.graphics.getDeltaTime());
-		truck2.update(Gdx.graphics.getDeltaTime());
-		truck1.draw(batch);
-		truck2.draw(batch);
-		batch.end();
+		switchTrucks();
 	}
 
+	private void switchTrucks() {
+		if (Gdx.input.isKeyPressed(Keys.NUM_1)) {
+			currentTruck.setColor(Color.WHITE);
+			currentTruck = truck1;
+			currentTruck.setColor(Color.RED);
+		}
+		if (Gdx.input.isKeyPressed(Keys.NUM_2)) {
+			currentTruck.setColor(Color.WHITE);
+			currentTruck = truck2;
+			currentTruck.setColor(Color.RED);
+		}
+	}
 
 
 	@Override
