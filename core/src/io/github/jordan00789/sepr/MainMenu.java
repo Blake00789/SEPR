1c38e65b5e348d40a0983c4cfd5b3661a3fa8ed6
sch556 <sch556@york.ac.uk>
2020-01-14 00:27:53 +0000

    Refactored the Bullet class into the Projectile class.

--- core/src/io/github/jordan00789/sepr/MainMenu.java
+++ core/src/io/github/jordan00789/sepr/MainMenu.java
@@ -3,14 +3,10 @@ package io.github.jordan00789.sepr;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class MainMenu implements Screen {
 
